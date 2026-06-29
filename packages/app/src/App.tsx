5332e6ce4e08bb4791450bb86b67fd519653b0ce
Zhanna M <zhanna.myshkovskaya@gmail.com>
2026-06-29 11:26:56 +0300

    Fix #21: feat: implement ScanProgress SSE component

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,11 +1,13 @@
 import GraphCanvas from './components/GraphCanvas.js';
 import NodeDetail from './components/NodeDetail.js';
+import ScanProgress from './components/ScanProgress.js';
 import SearchBar from './components/SearchBar.js';
 
 export default function App() {
     return (
         <div className="flex h-screen w-screen overflow-hidden bg-background text-foreground">
             <div className="relative flex-1">
+                <ScanProgress />
                 <div className="absolute inset-x-0 top-4 z-10 flex justify-center px-4">
                     <SearchBar />
                 </div>
